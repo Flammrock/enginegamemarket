76cee1bb1381ff8377c6dccb4ad9daccd98d3bbf
Flammrock <flammrockgame@gmail.com>
2020-08-14 19:33:14 +0200

    init

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const port = process.env.PORT || 3000
 
 app.use(express.static('client'));
 
+
 app.get('/', (req, res) => {
   res.sendFile('index.html')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server HTTP listening at http://localhost:${port}`)
+  console.log(`Database endpoint at ${process.env.DATABASE_URL}`);
 })
