e47a1c6c26c28b3628ed013aacb321b8115cd9b0
Flammrock <flammrockgame@gmail.com>
2020-08-14 19:36:07 +0200

    init

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express')
+const pg = require('pg')
 const app = express()
 const port = process.env.PORT || 3000
+const databaseuri = process.env.DATABASE_URL;
+
+
+var client = new pg.Client(databaseuri);
+client.connect();
 
 
 app.use(express.static('client'));
-
-
 app.get('/', (req, res) => {
   res.sendFile('index.html')
 })
-
 app.listen(port, () => {
   console.log(`Server HTTP listening at http://localhost:${port}`)
-  console.log(`Database endpoint at ${process.env.DATABASE_URL}`);
+  console.log(`Database endpoint at ${databaseuri}`);
 })
